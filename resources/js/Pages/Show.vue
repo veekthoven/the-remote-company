2cdbf2fd36fb471aee1f4f69a157d681ce366c51
Victor Abbah Nkoms <veekthoven@gmail.com>
2022-07-19 22:11:26 +0100

    Add show page

---
+++ resources/js/Pages/Show.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { Head, Link } from '@inertiajs/inertia-vue3';
+import GuestLayout from '@/Layouts/GuestLayout.vue';
+import Pdf from '@/Components/View/Pdf.vue';
+import Code from '@/Components/View/Code.vue';
+
+const props = defineProps({
+    resource: Object,
+})
+
+</script>
+
+<template>
+    <Head title="Welcome" />
+
+    <GuestLayout>
+        <div>
+            <Pdf :resource="resource" v-if="resource.type === 'pdf'" />
+            <Code :resource="resource" v-if="resource.type === 'code'" />
+        </div>
+    </GuestLayout>
+</template>
