703273a2e85190d11feef1d1409db1cd352c167e
Victor Abbah Nkoms <veekthoven@gmail.com>
2022-07-19 22:07:32 +0100

    Create ResourceController and route handlers

---
+++ app/Http/Controllers/ResourcesController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Enums\Type;
+use Inertia\Inertia;
+use App\Models\Resource;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Redirect;
+
+class ResourcesController extends Controller
+{
+    public function home()
+    {   
+        return Inertia::render('Home', [
+            'resources' => Resource::latest()->paginate(10)
+        ]);
+    }
+
+    public function admin()
+    {
+        return Inertia::render('Admin', [
+            'allResources' => Resource::latest()->paginate(10)
+        ]);
+    }
+
+    public function show(Request $request, Resource $resource)
+    {
+        return Inertia::render('Show', [
+            'resource' => $resource
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate(config("app.rules.{$request->type}"));
+
+        Resource::create([
+            'title' => $data['title'],
+            'type' => $this->getTypeFromRequest($request),
+            'body' => $this->prepareBody($request)
+        ]);
+
+        return Redirect::route('home');
+    }
+
+    protected function getTypeFromRequest($request)
+    {
+        if ($request->type === 'pdf') {
+            return Type::PDF->value;
+        }
+
+        if ($request->type === 'code') {
+            return Type::CODE->value;
+        }
+
+        if ($request->type === 'url') {
+            return Type::URL->value;
+        }
+    }
+
+    protected function prepareBody($request)
+    {
+        if ($request->type === 'pdf') {
+            $path = $request->file('file')->store('pdfs');
+
+            return [
+                'file' => $path
+            ];
+        }
+
+        if ($request->type === 'url') {
+            return [
+                'link' => $request->link,
+                'new_tab' => $request->new_tab
+            ];
+        }
+
+        if ($request->type === 'code') {
+            return [
+                'code' => $request->code,
+                'description' => $request->description
+            ];
+        }
+    }
+}
