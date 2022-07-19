a46daf1ccebc9e92440db9fd18c7935dccdd4145
Victor Abbah Nkoms <veekthoven@gmail.com>
2022-07-19 22:09:09 +0100

    Add resource migration

---
+++ database/migrations/2022_07_18_133330_create_resources_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Enums\Type;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('resources', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->enum('type', [
+                Type::PDF->value,
+                Type::CODE->value,
+                Type::URL->value,
+            ]);
+            $table->json('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('resources');
+    }
+};
