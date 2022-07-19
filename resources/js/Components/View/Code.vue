175b040478c550d401c4aba73d4b5bd5d6998656
Victor Abbah Nkoms <veekthoven@gmail.com>
2022-07-19 22:09:54 +0100

    Add code view component

---
+++ resources/js/Components/View/Code.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        {{ resource.body.code }}
+    </div>
+</template>
+
+<script setup>
+  import { useForm } from '@inertiajs/inertia-vue3'
+  import { component as VueCodeHighlight } from 'vue-code-highlight';
+
+  const props = defineProps({
+      resource: Object,
+  })
+
+  const form = useForm({
+    title: null,
+    description: null,
+    code: null,
+    type: 'code'
+  })
+
+  function submit() {
+    form.post('/resources')
+  }
+
+</script>
