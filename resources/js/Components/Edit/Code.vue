89a97fb91c31ea6482dd15eda4eaf17df4fd1be0
Victor Abbah Nkoms <veekthoven@gmail.com>
2022-07-20 11:45:41 +0100

    Add edit views

---
+++ resources/js/Components/Edit/Code.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <div>
+            <div class="sm:col-span-3 mb-5">
+                <label for="first-name" class="block text-sm font-medium text-gray-900">Title</label>
+                <div class="mt-1">
+                    <input v-model="form.title" type="text" name="title" id="title" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" />
+                    <div class="text-red-500 text-sm mt-1" v-if="form.errors.title">{{ form.errors.title }}</div>
+                </div>
+            </div>
+
+            <label for="description" class="block text-sm font-medium text-gray-700">Description</label>
+            <div class="mt-1 mb-5">
+              <textarea v-model="form.description" id="description" name="description" rows="3" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border border-gray-300 rounded-md" />
+              <div class="text-red-500 text-sm mt-1" v-if="form.errors.description">{{ form.errors.description }}</div>
+            </div>
+
+            <label for="code" class="block text-sm font-medium text-gray-700">Code</label>
+            <div class="mt-1">
+              <textarea v-model="form.code" id="code" name="code" rows="3" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border border-gray-300 rounded-md" />
+              <div class="text-red-500 text-sm mt-1" v-if="form.errors.code">{{ form.errors.code }}</div>
+            </div>
+
+            <div class="mt-4 sm:flex-none">
+                <button @click="submit" type="button" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">Submit</button>
+            </div>
+          </div>
+    </div>
+</template>
+
+<script setup>
+  import { useForm } from '@inertiajs/inertia-vue3'
+  import { defineEmits } from "vue";
+
+  const emit = defineEmits(['success'])
+
+  const props = defineProps({
+      resource: Object,
+  })
+
+  const { resource } = props;
+
+
+  const form = useForm({
+    title: resource.title,
+    description: resource.body.description,
+    code: resource.body.code,
+    type: 'code'
+  })
+
+  function submit() {
+    form.post(`/resources/${resource.id}`, {
+      onSuccess: () => {
+        emit('success')
+      }
+    })
+  }
+
+</script>
---
+++ resources/js/Components/Edit/Link.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+        <div>
+            <div class="sm:col-span-3 mb-5">
+                <label for="title" class="block text-sm font-medium text-gray-900">Title</label>
+                <div class="mt-1">
+                    <input v-model="form.title" type="text" name="title" id="title" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" />
+                    <div class="text-red-500 text-sm mt-1" v-if="form.errors.title">{{ form.errors.title }}</div>
+                </div>
+            </div>
+
+            <div class="sm:col-span-3 mb-5">
+                <label for="link" class="block text-sm font-medium text-gray-900">Link</label>
+                <div class="mt-1">
+                    <input v-model="form.link" type="text" name="link" id="link" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" />
+                    <div class="text-red-500 text-sm mt-1" v-if="form.errors.link">{{ form.errors.link }}</div>
+                </div>
+            </div>
+
+            <div class="relative flex items-start">
+              <div class="flex items-center h-5">
+                <input v-model="form.new_tab" id="open" name="open" type="checkbox" class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded" />
+                <div class="text-red-500 text-sm mt-1" v-if="form.errors.new_tab">{{ form.errors.new_tab }}</div>
+              </div>
+              <div class="ml-3 text-sm">
+                <label for="open" class="font-bold text-gray-900">Open on a new tab</label>
+              </div>
+            </div>
+
+            <div class="mt-4 sm:flex-none">
+                <button @click="submit" type="button" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">Submit</button>
+            </div>
+          </div>
+    </div>
+</template>
+
+<script setup>
+  import { useForm } from '@inertiajs/inertia-vue3'
+  import { defineEmits } from "vue";
+
+  const emit = defineEmits(['success'])
+
+  const props = defineProps({
+      resource: Object,
+  })
+
+  const { resource } = props;
+
+  const form = useForm({
+    title: resource.title,
+    link: resource.body.link,
+    new_tab: resource.body.new_tab,
+    type: 'url'
+  })
+
+  function submit() {
+    form.post(`/resources/${resource.id}`, {
+      onSuccess: () => {
+        emit('success')
+      }
+    })
+  }
+
+</script>
