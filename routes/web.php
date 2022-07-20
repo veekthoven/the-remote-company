b35df18523a45adf315547eb2e77ef189eefb976
Victor Abbah Nkoms <veekthoven@gmail.com>
2022-07-20 11:48:06 +0100

    Add update and delete routes.

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,9 @@ Route::get('/admin', [ResourcesController::class, 'admin'])->name('admin');
 Route::get('/resources/{resource}', [ResourcesController::class, 'show']);
 
 Route::post('/resources', [ResourcesController::class, 'store']);
+
+Route::delete('/resources/{resource}', [ResourcesController::class, 'destroy']);
+
+Route::get('/resources/edit/{resource}', [ResourcesController::class, 'edit']);
+
+Route::post('/resources/{resource}', [ResourcesController::class, 'update']);
